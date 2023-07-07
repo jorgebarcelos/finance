1b0336115d598622b5db44f449cae1a8ddd55733
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2023-07-06 23:09:23 -0300

    dynamic template, views account change

--- perfil/urls.py
+++ perfil/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('gerenciar/', views.gerenciar, name="gerenciar"),
+    path('cadastrar_banco/', views.cadastrar_banco, name="cadastrar_banco"),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.messages import constants
+from django.contrib import messages
+from .models import Conta
 
 # Create your views here.
+
 
 def home(request):
     return render(request, "home.html")
 
+
 def gerenciar(request):
     return render(request, "gerenciar.html")
+
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('/perfil/gerenciar/')
+
+    conta = Conta(
+        apelido=apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+    conta.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso!')
+    return redirect('/perfil/gerenciar/')
